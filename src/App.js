ccc8763d5360e06b7551f41c7d7fb80438f4cd6f
yurisokolovicz <yurisokolovicz@gmail.com>
2023-04-25 10:43:12 -0300

    Exploring an Alternative Way of Defining Routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import Homepage from './pages/Home';
 import ProductsPage from './pages/Products';
 
-// We created the router object containing the path and the component to render when that path is active.
-// The path is the part after the domain name.
-// The information after the path is the component that will be rendered when that router is active.
-// https://www.example.com/products - "products" is the path
-// https://www.example.com/products/123 - "products/123" is the path
-// https://www.example.com/ - "/" is also the path (the route for the home page)
+// const routeDefinitions = createRoutesFromElements(
+//     <Route>
+//         <Route path="/" element={<Homepage />} />
+//         <Route path="/products" element={<ProductsPage />} />
+//     </Route>
+// );
 
 const router1 = createBrowserRouter([
     { path: '/', element: <Homepage /> },
     { path: '/products', element: <ProductsPage /> }
 ]);
+
+// const router1 = createBrowserRouter(routeDefinitions);
 
 function App() {
     return <RouterProvider router={router1} />;
