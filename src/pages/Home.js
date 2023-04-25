7a1bbc1b8e5e1b29e43004a82cd66fb200fdf138
yurisokolovicz <yurisokolovicz@gmail.com>
2023-04-25 10:53:44 -0300

    Navigating between Pages with Links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Homepage = () => {
-    return <h>My Home Page</h>;
+    return (
+        <>
+            <h>My Home Page</h>
+            <p>
+                Go to <Link to="/products">the list of products</Link>
+            </p>
+        </>
+    );
 };
 
 export default Homepage;
